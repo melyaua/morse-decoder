9f741935097664bdf1eca1cfa8d00fc5971b1b37
melyaua <igor.melnychenko@tmx-learning.com>
2020-10-09 14:44:31 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    // Разбиваем строку на массив по 10 элементов
+    let arr = [];
+    for (let i=0; i < expr.length; i += 10) {
+        arr.push(expr.substr(i, 10));
+    }
+
+    // Перебираем каждый элемент массива
+    let str = '';
+    for (let i of arr) {
+        if (i == '**********') {
+            // Пробел добавляем к выходящей строке
+            str += ' ';
+        } else {
+            let line = i.split('').join('');
+            // Удаляем нули в начале строки
+            line = line.replace(/00/ig,'');
+            // Преобразовываем каждую пару цифр из элемента массива в . или -
+            let currSymbol = '';
+            for (let j=0; j< line.length; j += 2) {
+                currSymbol += (line.substr(j, 2) == '10') ? '.' : '-';
+            }
+            // Добавляем к результату значение из основной таблицы
+            str += MORSE_TABLE[currSymbol];
+        }
+    }
+    return str;
 }
 
 module.exports = {
